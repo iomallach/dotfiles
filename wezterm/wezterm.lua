d2c20a04ebe19e1444a42022297e9040c3cbafbf
Alexander Butenko <a.butenko.o@gmail.com>
2024-04-21 19:29:11 +0200

    Add sessionizer and wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Zenburn"
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 16
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+return config
