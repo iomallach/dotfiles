950c795acfcdf5049fd958a7575b539367093d0b
Alexander Butenko <a.butenko.o@gmail.com>
2024-05-16 06:58:03 +0200

    Update tmux, wezterm, yabai

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Zenburn"
+-- config.color_scheme = "Zenburn"
+-- config.color_scheme = "DoomOne"
+config.color_scheme = "Catppuccin Macchiato"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
@@ -11,5 +13,6 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
+config.window_decorations = "RESIZE"
 
 return config
