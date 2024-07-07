abe936786c3d76bec742a7ee7ba6ecb1d6a4b2c4
Alexander Butenko <a.butenko.o@gmail.com>
2024-07-07 21:58:32 +0200

    Add firenvim, enable italic in tmux

--- .config/nvim/lua/config/globals.lua
+++ .config/nvim/lua/config/globals.lua
@@ -3,9 +3,13 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 
 -- disable some default providers
 for _, provider in ipairs({ "node", "perl", "python3", "ruby" }) do
-	vim.g["loaded_" .. provider .. "_provider"] = 0
+  vim.g["loaded_" .. provider .. "_provider"] = 0
 end
 
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.fn.has("win32") ~= 0
 vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
+
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
+end
