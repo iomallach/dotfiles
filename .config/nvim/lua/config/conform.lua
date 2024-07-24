30579e8422fd46e6f08bd12dc942dc2db074b85f
Alexander Butenko <a.butenko.o@gmail.com>
2024-07-24 21:40:02 +0200

    In confrom and nvim-lint use executable from venv if available

--- .config/nvim/lua/config/conform.lua
+++ .config/nvim/lua/config/conform.lua
@@ -1,37 +1,48 @@
-local options = {
-	lsp_fallback = true,
-	formatters = {
-		ruff_fix = {
-			command = "ruff",
-			args = {
-				"check",
-				"--fix",
-				"-e",
-				"-n",
-				"--select",
-				"I",
-				"--stdin-filename",
-				"$FILENAME",
-				"-",
-			},
-			stdin = true,
-			cwd = require("conform.util").root_file({
-				"pyproject.toml",
-				"ruff.toml",
-			}),
-		},
-	},
+local function get_ruff_executable()
+  local is_ruff_in_venv = vim.fn.getcwd() .. "/.venv/bin/ruff"
+  if is_ruff_in_venv then
+    return vim.fn.getcwd() .. "/.venv/bin/ruff"
+  end
+  return "ruff"
+end
 
-	formatters_by_ft = {
-		lua = { "stylua" },
-		c = { "clang_format" },
-		python = { "ruff_format", "ruff_fix" },
-	},
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+local options = {
+  lsp_fallback = true,
+  formatters = {
+    ruff_fix = {
+      command = get_ruff_executable(),
+      args = {
+        "check",
+        "--fix",
+        "-e",
+        "-n",
+        "--select",
+        "I",
+        "--stdin-filename",
+        "$FILENAME",
+        "-",
+      },
+      stdin = true,
+      cwd = require("conform.util").root_file({
+        "pyproject.toml",
+        "ruff.toml",
+      }),
+    },
+    ruff_format = {
+      command = get_ruff_executable(),
+    }
+  },
+
+  formatters_by_ft = {
+    lua = { "stylua" },
+    c = { "clang_format" },
+    python = { "ruff_format", "ruff_fix" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -2,7 +2,23 @@ return {
   "mfussenegger/nvim-lint",
   lazy = false,
   config = function()
-    require("lint").linters_by_ft = {
+    local lint = require("lint")
+    local mypy = lint.linters.mypy
+    local ruff = lint.linters.ruff
+    local cwd = vim.fn.getcwd()
+
+    -- If available in venv, use from venv
+    local mypy_exists_in_venv = vim.fn.filereadable(cwd .. "/.venv/bin/mypy")
+    if mypy_exists_in_venv then
+      mypy.cmd = cwd .. "/.venv/bin/mypy"
+    end
+
+    local ruff_exists_in_venv = vim.fn.filereadable(cwd .. "/.venv/bin/ruff")
+    if ruff_exists_in_venv then
+      ruff.cmd = cwd .. "/.venv/bin/ruff"
+    end
+
+    lint.linters_by_ft = {
       c = { "clangtidy" },
       python = { "mypy", "ruff" },
       dockerfile = { "hadolint" },
