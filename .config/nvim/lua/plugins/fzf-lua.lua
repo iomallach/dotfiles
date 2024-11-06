d103b3786c6494e59a18815391497f485074d17d
Alexander Butenko <a.butenko.o@gmail.com>
2024-11-06 21:56:24 +0100

    Add missing CR

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -4,6 +4,11 @@ return {
 	init = function()
 		local keymap = require("vim.keymap")
 		keymap.set("n", "<leader>fg", "<CMD>FzfLua live_grep<CR>", { desc = "Fzf Live Grep" })
-		keymap.set("n", "<leader>fws", "<CMD>FzfLua live_workspace_symbols", { desc = "Fzf Live Workspace symbols" })
+		keymap.set(
+			"n",
+			"<leader>fws",
+			"<CMD>FzfLua live_workspace_symbols<CR>",
+			{ desc = "Fzf Live Workspace symbols" }
+		)
 	end,
 }
