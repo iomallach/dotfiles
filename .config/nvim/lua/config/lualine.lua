f6539184561fd6a630253db59ca50ac0f922c3bf
Alexander Butenko <a.butenko.o@gmail.com>
2024-11-10 09:04:05 +0100

    Add "recording" back to lualine

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -1,60 +1,71 @@
 local M = {}
 
 M.stbufnr = function()
-  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+	return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
 end
 
 M.lsp_info = function()
-  if rawget(vim, "lsp") then
-    for _, client in ipairs(vim.lsp.get_active_clients()) do
-      if client.attached_buffers[M.stbufnr()] and client.name ~= "null-ls" then
-        return (vim.o.columns > 100 and "   LSP ~ " .. client.name .. " ") or "   LSP "
-      end
-    end
-  end
+	if rawget(vim, "lsp") then
+		for _, client in ipairs(vim.lsp.get_active_clients()) do
+			if client.attached_buffers[M.stbufnr()] and client.name ~= "null-ls" then
+				return (vim.o.columns > 100 and "   LSP ~ " .. client.name .. " ") or "   LSP "
+			end
+		end
+	end
 
-  return ""
+	return ""
 end
 
 M.working_dir = function()
-  local components = {}
-  for component in vim.fn.getcwd():gmatch("[^/]+") do
-    table.insert(components, component)
-  end
-  return " " .. components[#components] .. " "
+	local components = {}
+	for component in vim.fn.getcwd():gmatch("[^/]+") do
+		table.insert(components, component)
+	end
+	return " " .. components[#components] .. " "
 end
 
 M.config = function()
-  -- ""
-  require("lualine").setup({
-    options = {
-      theme = "auto",
-      section_separators = { left = "", right = "" },
-      component_separators = {},
-      globalstatus = true,
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { { "filetype", icon_only = true, icon = { align = "right" } }, "filename" },
-      lualine_c = { { "branch", icon = "" }, { "diff", symbols = { added = " ", modified = " ", removed = " " } } },
-      lualine_x = { "diagnostics", M.lsp_info },
-      lualine_y = {
-        {
-          M.working_dir,
-          padding = 0,
-        },
-      },
-      lualine_z = {
-        {
-          "progress",
-          icons_enabled = true,
-          icon = "󰗚",
-          padding = 0,
-        },
-        "location",
-      },
-    },
-  })
+	-- ""
+	require("lualine").setup({
+		options = {
+			theme = "auto",
+			section_separators = { left = "", right = "" },
+			component_separators = {},
+			globalstatus = true,
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { { "filetype", icon_only = true, icon = { align = "right" } }, "filename" },
+			lualine_c = {
+				{ "branch", icon = "" },
+				{ "diff", symbols = { added = " ", modified = " ", removed = " " } },
+			},
+			lualine_x = {
+				{
+					require("noice").api.status.mode.get,
+					cond = require("noice").api.status.mode.has,
+					color = { fg = "#ff9e64" },
+				},
+				"diagnostics",
+				M.lsp_info,
+			},
+			lualine_y = {
+				{
+					M.working_dir,
+					padding = 0,
+				},
+			},
+			lualine_z = {
+				{
+					"progress",
+					icons_enabled = true,
+					icon = "󰗚",
+					padding = 0,
+				},
+				"location",
+			},
+		},
+	})
 end
 
 return M
