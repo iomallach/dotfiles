4596e376f1615b7561c252b2105ae803e8863d0a
Alexander Butenko <a.butenko.o@gmail.com>
2024-11-11 08:23:51 +0100

    Remove rust.vim, leave only rustacean

--- .config/nvim/lua/plugins/lang-presets.lua
+++ .config/nvim/lua/plugins/lang-presets.lua
@@ -5,13 +5,6 @@ return {
 		ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
 	},
 	{
-		"rust-lang/rust.vim",
-		ft = "rust",
-		init = function()
-			vim.g.rustfmt_autosave = 1
-		end,
-	},
-	{
 		"mrcjkb/rustaceanvim",
 		version = "^4",
 		ft = { "rust" },
