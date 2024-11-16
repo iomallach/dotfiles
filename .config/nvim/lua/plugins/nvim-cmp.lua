d431d02bf6096cb11f2ccd548b9e2ad9b7fd83e6
Alexander Butenko <a.butenko.o@gmail.com>
2024-11-16 13:56:54 +0100

    Extend snippets

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -97,7 +97,18 @@ return {
 		{
 			-- snippet plugin
 			"L3MON4D3/LuaSnip",
-			dependencies = "rafamadriz/friendly-snippets",
+			build = function()
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end,
+			dependencies = {
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
 		},
 
 		-- autopairing of (){}[] etc
