0e39d7df8b34126ac7c79100a9639b9984bd25ce
Alexander Butenko <a.butenko.o@gmail.com>
2025-07-19 13:12:23 +0200

    Change conform config such that it greedily looks for black first, then falls back to ruff always

--- .config/nvim/lua/config/conform.lua
+++ .config/nvim/lua/config/conform.lua
@@ -1,17 +1,34 @@
-local function get_ruff_executable()
-	local path_to_ruff = vim.fn.getcwd() .. "/.venv/bin/ruff"
-	local is_ruff_in_venv = vim.fn.filereadable(path_to_ruff)
-	if is_ruff_in_venv == 1 then
-		return path_to_ruff
+local function get_python_formatter_executable()
+	-- one of these must be either installed in the venv or installed globally
+	local formatters = { "black", "ruff" }
+
+	for _, formatter in ipairs(formatters) do
+		if vim.fn.executable(formatter) == 1 then
+			return formatter
+		end
 	end
+
+	-- fallback to ruff always
 	return "ruff"
+end
+
+local function get_python_formatters()
+	local formatter = get_python_formatter_executable()
+
+	if formatter == "ruff" then
+		return { "ruff_format", "ruff_fix" }
+	elseif formatter == "black" then
+		return { "black_format" }
+	end
+
+	return { "ruff_format", "ruff_fix" } -- fallback
 end
 
 local options = {
 	lsp_fallback = true,
 	formatters = {
 		ruff_fix = {
-			command = get_ruff_executable(),
+			command = "ruff",
 			args = {
 				"check",
 				"--fix",
@@ -30,14 +47,35 @@ local options = {
 			}),
 		},
 		ruff_format = {
-			command = get_ruff_executable(),
+			command = "ruff",
+			args = {
+				"format",
+				"--force-exclude",
+				"--stdin-filename",
+				"$FILENAME",
+				"-",
+			},
+			stdin = true,
+			cwd = require("conform.util").root_file({
+				"pyproject.toml",
+				"ruff.toml",
+			}),
+		},
+		black_format = {
+			command = "black",
+			args = {
+				"--stdin-filename",
+				"$FILENAME",
+				"--quiet",
+				"-",
+			},
 		},
 	},
 
 	formatters_by_ft = {
 		lua = { "stylua" },
 		c = { "clang_format" },
-		python = { "ruff_format", "ruff_fix" },
+		python = get_python_formatters(),
 		go = { "gofumpt", "goimports" },
 		java = { "google-java-format" },
 	},
