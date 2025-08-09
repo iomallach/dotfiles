2217a9958db885874b7edff0ed7c73d81e3d334e
Alexander Butenko <a.butenko.o@gmail.com>
2025-08-09 15:25:26 +0200

    Fix annoying deprecation warnings with sign_define

--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -9,10 +9,28 @@ local diagnostic_signs = {
 }
 
 local setup_diagnostic_signs = function()
-	for type, icon in pairs(diagnostic_signs) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-	end
+	vim.diagnostic.config({
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+				[vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+				[vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+				[vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+			},
+			texthl = {
+				[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+				[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+				[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+				[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+			},
+			humhl = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN] = "",
+				[vim.diagnostic.severity.HINT] = "",
+				[vim.diagnostic.severity.INFO] = "",
+			},
+		},
+	})
 end
 
 local setup_pylyzer = function(capabilities)
