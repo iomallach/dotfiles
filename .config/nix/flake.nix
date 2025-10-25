f99f3787699b6016fdfd6aefdf5a004e34298c78
Alexander Butenko <a.butenko.o@gmail.com>
2025-10-25 19:22:53 +0200

    Include nix dev tools, reformat the flake

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -21,99 +21,119 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, neovim-nightly, nix-homebrew, ... }:
-  let
-    configuration = { pkgs, config, ... }: {
-      nixpkgs.config.allowUnfree = true;
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        with pkgs ; [ 
-          mkalias
-          vim
-          neovim-nightly.packages.${pkgs.system}.default
-          qmk # split keyboard
-          fzf
-          eza
-          asciiquarium
-          zed-editor
-          uv
-          nix-search-tv
-          figlet # ascii prints
-          bat
-          jq
-          cmatrix
-          curl
-          wget
-          blueutil
-          btop
-          fd
-          helix
-          lazygit
-          gitu
-          # neovide it is a cask within brew
-          nowplaying-cli
-          switchaudio-osx
-          yazi
-          openvpn
-          ripgrep
-          starship # prompt
-          tmux
-          tldr
-          zoxide
-          (lua54Packages.lua.withPackages (ps: with ps; [ luasocket cjson luasec ]))
-          ghostty-bin
-          # sst/tap/opencode - cask within brew
-        ];
-      # From brew formulaes
-      # ifstat
-      # nasm
+  outputs =
+    {
+      self,
+      nix-darwin,
+      nixpkgs,
+      neovim-nightly,
+      nix-homebrew,
+      ...
+    }:
+    let
+      configuration =
+        { pkgs, config, ... }:
+        {
+          nixpkgs.config.allowUnfree = true;
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            mkalias
+            vim
+            neovim-nightly.packages.${pkgs.system}.default
+            qmk # split keyboard
+            fzf
+            eza
+            asciiquarium
+            zed-editor
+            uv
+            nix-search-tv
+            figlet # ascii prints
+            bat
+            jq
+            cmatrix
+            curl
+            wget
+            blueutil
+            btop
+            fd
+            helix
+            lazygit
+            gitu
+            # neovide it is a cask within brew
+            nowplaying-cli
+            switchaudio-osx
+            yazi
+            openvpn
+            ripgrep
+            starship # prompt
+            tmux
+            tldr
+            zoxide
+            (lua54Packages.lua.withPackages (
+              ps: with ps; [
+                luasocket
+                cjson
+                luasec
+              ]
+            ))
+            ghostty-bin
+            # nix dev
+            nixfmt-rfc-style # formatter
+            nil # lsp
+            nixd # lsp 2
+            # sst/tap/opencode - cask within brew
+          ];
+          # From brew formulaes
+          # ifstat
+          # nasm
 
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
+          system.activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+              };
+            in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read -r src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
+          # Add Homebrew to PATH
+          # environment.systemPath = [ "/opt/homebrew/bin" "/opt/homebrew/sbin" ];
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
         };
-      in
-        pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
-      # Add Homebrew to PATH
-      # environment.systemPath = [ "/opt/homebrew/bin" "/opt/homebrew/sbin" ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."macbookair" = nix-darwin.lib.darwinSystem {
-      modules = [
-          configuration 
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."macbookair" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
           nix-homebrew.darwinModules.nix-homebrew
           {
             nix-homebrew = {
@@ -130,7 +150,7 @@
               autoMigrate = true;
             };
           }
-      ];
+        ];
+      };
     };
-  };
 }
