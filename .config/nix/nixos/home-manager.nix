c98b0981743601e618671035a7d2c650c915889a
Alexander Butenko <a.butenko.o@gmail.com>
2026-03-20 20:04:19 +0100

    (nix) manage git config via home manager

--- .config/nix/nixos/home-manager.nix
+++ .config/nix/nixos/home-manager.nix
@@ -12,6 +12,19 @@
 
   programs.home-manager.enable = true;
 
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Alexander Butenko";
+      user.email = "a.butenko.o@gmail.com";
+      merge.tool = "codediff";
+      "mergetool \"codediff\"".cmd = "nvim \"$MERGED\" -c \"CodeDiff merge \\\"$MERGED\\\"\"";
+      diff.tool = "codediff";
+      "difftool \"codediff\"".cmd = "nvim \"$LOCAL\" \"$REMOTE\" +\"CodeDiff file $LOCAL $REMOTE\"";
+      init.defaultBranch = "main";
+    };
+  };
+
   imports = [
     inputs.spicetify-nix.homeManagerModules.default
   ];
