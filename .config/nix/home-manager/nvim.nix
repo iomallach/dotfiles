0623e022f4f0007e26acc70a4f75f8643b163cea
Alexander Butenko <a.butenko.o@gmail.com>
2026-04-03 13:41:16 +0200

    (nvim) use absolute path for nvim's out of store symlink

--- .config/nix/home-manager/nvim.nix
+++ .config/nix/home-manager/nvim.nix
@@ -1,5 +1,6 @@
 { config, ... }:
 
 {
-  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink (toString ../../nvim);
+  xdg.configFile."nvim".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/nvim";
 }
