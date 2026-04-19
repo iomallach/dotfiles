fe846568fdfbd58436dd75f64b4a4a61c1b7de55
Alexander Butenko <a.butenko.o@gmail.com>
2026-04-19 13:10:52 +0200

    (nix) clean up tuxbook module

--- .config/nix/modules/hosts/tuxbook.nix
+++ .config/nix/modules/hosts/tuxbook.nix
@@ -34,10 +34,6 @@ let
       {
         environment.sessionVariables = {
           GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
-          XCURSOR_THEME = "catppuccin-macchiato-blue-cursors";
-          XCURSOR_SIZE = "35";
-          HYPRCURSOR_THEME = "catppuccin-macchiato-blue-cursors";
-          HYPRCURSOR_SIZE = "35";
         };
 
         environment.systemPackages = [
