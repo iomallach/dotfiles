1c7584700776044bfae98107bdde962f01eb43c6
Alexander Butenko <a.butenko.o@gmail.com>
2026-04-19 13:02:40 +0200

    (nix) refactor module passing in tuxbook

--- .config/nix/modules/hosts/tuxbook.nix
+++ .config/nix/modules/hosts/tuxbook.nix
@@ -3,67 +3,84 @@
   inputs,
   ...
 }:
+let
+  externalModules = [
+    inputs.tuxedo-nixos.nixosModules.default
+    inputs.catppuccin.nixosModules.catppuccin
+    inputs.home-manager.nixosModules.home-manager
+  ];
+  genericModules = with config.flake.modules.generic; [
+    nix
+    fonts
+    gaming
+    languages
+    shell
+    iomallach
+  ];
+  nixosModules = with config.flake.modules.nixos; [
+    boot
+    browsers
+    hardware
+    media
+    networking
+    services
+    systemd
+    desktopGui
+    vpn
+  ];
+  hostBoundModules = [
+    (
+      { pkgs, ... }:
+      {
+        environment.sessionVariables = {
+          GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
+          XCURSOR_THEME = "catppuccin-macchiato-blue-cursors";
+          XCURSOR_SIZE = "35";
+          HYPRCURSOR_THEME = "catppuccin-macchiato-blue-cursors";
+          HYPRCURSOR_SIZE = "35";
+        };
+
+        environment.systemPackages = [
+          pkgs.powertop
+        ];
+      }
+    )
+  ];
+  externalHomeManagerModules = [
+    inputs.spicetify-nix.homeManagerModules.default
+    inputs.dms.homeModules.dank-material-shell
+  ];
+  homeManagerModules = with config.flake.modules.homeManager; [
+    xdgConfigs
+    iomallach
+    editors
+    terminals
+    anki
+    git
+    pdf
+    shell
+    spotify
+    ai
+  ];
+in
 {
   flake.nixosConfigurations.tuxbook = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = with config.flake.modules; [
-      inputs.tuxedo-nixos.nixosModules.default
-      inputs.catppuccin.nixosModules.catppuccin
-      inputs.home-manager.nixosModules.home-manager
-      generic.nix
-      nixos.boot
-      nixos.browsers
-      generic.fonts
-      generic.gaming
-      generic.languages
-      nixos.hardware
-      nixos.media
-      nixos.networking
-      nixos.services
-      generic.shell
-      nixos.systemd
-      generic.iomallach
-      nixos.desktopGui
-      nixos.vpn
-      (
-        { pkgs, ... }:
+    modules =
+      externalModules
+      ++ genericModules
+      ++ nixosModules
+      ++ hostBoundModules
+      ++ [
         {
-          environment.sessionVariables = {
-            GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
-            XCURSOR_THEME = "catppuccin-macchiato-blue-cursors";
-            XCURSOR_SIZE = "35";
-            HYPRCURSOR_THEME = "catppuccin-macchiato-blue-cursors";
-            HYPRCURSOR_SIZE = "35";
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.iomallach = {
+              imports = externalHomeManagerModules ++ homeManagerModules;
+            };
           };
-
-          environment.systemPackages = [
-            pkgs.powertop
-          ];
         }
-      )
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          users.iomallach = {
-            imports = with config.flake.modules.homeManager; [
-              inputs.spicetify-nix.homeManagerModules.default
-              inputs.dms.homeModules.dank-material-shell
-
-              xdgConfigs
-              iomallach
-              editors
-              terminals
-              anki
-              git
-              pdf
-              shell
-              spotify
-              ai
-            ];
-          };
-        };
-      }
-    ];
+      ];
   };
 }
