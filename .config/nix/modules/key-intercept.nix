f21baeac9815dda8456a6b26fc61cde59859fdb3
Alexander Butenko <a.butenko.o@gmail.com>
2026-06-30 20:49:53 +0200

    (nix) tuxbook fixes

--- .config/nix/modules/key-intercept.nix
+++ .config/nix/modules/key-intercept.nix
@@ -18,6 +18,10 @@
     {
       hardware.uinput.enable = true;
 
+      services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="input", KERNEL=="event*", ATTRS{name}=="AULA-F75 5.0 KB Keyboard", ATTRS{uniq}=="ca:cb:2c:9e:29:93", SYMLINK+="input/by-id/aula-f75-bluetooth-event-kbd", TAG+="systemd", ENV{SYSTEMD_WANTS}+="kanata-restart.service"
+      '';
+
       systemd.services.kanata = {
         description = "Kanata keyboard remapper";
         wantedBy = [ "multi-user.target" ];
@@ -30,6 +34,14 @@
           '';
           Restart = "on-failure";
           RestartSec = 3;
+        };
+      };
+
+      systemd.services.kanata-restart = {
+        description = "Restart Kanata when Aula F75 reconnects";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.systemd}/bin/systemctl try-restart kanata.service";
         };
       };
     };
